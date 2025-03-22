7ca290ffd0ab0caac8a5254d636de71397718662
Mike Nuzzolo <michael.nuzzolo@gmail.com>
2025-03-22 11:49:24 -0500

    Updating ContentView styling

--- Future-Take-Home/Future-Take-Home/Views/ContentView.swift
+++ Future-Take-Home/Future-Take-Home/Views/ContentView.swift
@@ -14,22 +14,39 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             List {
-                Section {
+                headerView
+                Section("Exercises") {
+                    ForEach(Array(controller.getSortedExercises()), id: \.self) { exercise in
+                        NavigationLink {
+                            ExerciseDetailView(viewModel: ExerciseViewModel(sets: exercise))
+                        } label: {
+                            ExerciseListView(viewModel: ExerciseViewModel(sets: exercise))
+                        }
+                    }
+                }
+            }
+            .navigationTitle(Text("Your Exercises"))
+        }
+    }
+    
+    @ViewBuilder
+    var headerView: some View {
+        Section("Energy") {
+            HStack {
+                Spacer()
+                VStack(alignment: .center) {
+                    Text("Great work!")
+                        .bold()
+                        .font(.system(size: 26))
+                        .padding(.bottom, 2)
                     HStack {
-                        Text("**\(controller.totalCaloriesBurned)** calories burned!")
+                        Text("**\(controller.totalCaloriesBurned)** calories burned")
                         Image(systemName: "heart.fill")
                             .foregroundStyle(.accent)
                     }
                 }
-                ForEach(Array(controller.getSortedExercises()), id: \.self) { exercise in
-                    NavigationLink {
-                        ExerciseDetailView(viewModel: ExerciseViewModel(sets: exercise))
-                    } label: {
-                        ExerciseListView(viewModel: ExerciseViewModel(sets: exercise))
-                    }
-                }
+                Spacer()
             }
-            .navigationTitle(Text("Exercises"))
         }
     }
 }
